113812dd7098a1823b787495b29afdbeb4db7dc3
Jason Cheung <jasoncheung@Jasons-MacBook-Pro.local>
2018-09-30 18:13:01 -0700

    c

--- s05/5.homework/homework.ts
+++ s05/5.homework/homework.ts
@@ -2,34 +2,34 @@ namespace TAE_SCHOOLtask{
     let tblContact = document.querySelector('#tblContact') as HTMLTableElement;
     let frmContact = document.querySelector('#frmContact') as HTMLFormElement;
     let  txtCourse = document.querySelector('#txtCourse') as HTMLInputElement;
-    let  txtAssignment1 = document.querySelector('#txtAssignment1') as HTMLInputElement;
-    let  txtAssignment2 = document.querySelector('#txtAssignment2') as HTMLInputElement;
-    let  txtAssignment3 = document.querySelector('#txtAssignment3') as HTMLInputElement;
-    let  txtAssignment4 = document.querySelector('#txtAssignment4') as HTMLInputElement;
-    let  txtAssignment5 = document.querySelector('#txtAssignment5') as HTMLInputElement;
+    let  txtTeacher = document.querySelector('#txtTeacher') as HTMLInputElement;
+    let  txtContacts = document.querySelector('#txtContacts') as HTMLInputElement;
+    let  txtPercentage = document.querySelector('#txtPercentage') as HTMLInputElement;
+    let  txtOptional = document.querySelector('#txtOptional') as HTMLInputElement;
+    let  txtOptional2 = document.querySelector('#txtOptional2') as HTMLInputElement;
 
 
     frmContact.onsubmit = () => {
         let Course = txtCourse.value;
-        let Assignment1 = txtAssignment1.value;
-        let Assignment2 = txtAssignment2.value;
-        let Assignment3 = txtAssignment3.value;
-        let Assignment4 = txtAssignment4.value;
-        let Assignment5 = txtAssignment5.value;
+        let Teacher = txtTeacher.value;
+        let Contacts = txtContacts.value;
+        let Percentage = txtPercentage.value;
+        let Optional = txtOptional.value;
+        let Optional2 = txtOptional2.value;
     
         let row = tblContact.insertRow()
         let cell = row.insertCell()
         cell.innerHTML = Course;
         cell = row.insertCell()
-        cell.innerHTML = Assignment1;
+        cell.innerHTML = Teacher;
         cell = row.insertCell()
-        cell.innerHTML = Assignment2;
+        cell.innerHTML = Contacts;
         cell = row.insertCell()
-        cell.innerHTML = Assignment3;
+        cell.innerHTML = Percentage;
         cell = row.insertCell()
-        cell.innerHTML = Assignment4;
+        cell.innerHTML = Optional;
         cell = row.insertCell()
-        cell.innerHTML = Assignment5;
+        cell.innerHTML = Optional2;
         cell = row.insertCell()
 
 
